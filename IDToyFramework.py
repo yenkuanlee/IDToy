dd03b0fb0e5d9c65357a2ff1f38fb7483610f7e8
Kevin Yen-Kuan Lee <yenkuanlee@gmail.com>
2018-10-17 15:59:27 +0800

    Now can set user info

--- IDToyFramework.py
+++ IDToyFramework.py
@@ -30,23 +30,35 @@ class IDToyFramework:
         contract_address = Jline['contract_address']
         self.contract_instance = self.w3.eth.contract(abi=self.abi, address=contract_address)
         self.NameHash = self.api.object_put(io.BytesIO(json.dumps({"Data":"Name"}).encode()))['Hash']
-        self.CountryHash = self.api.object_put(io.BytesIO(json.dumps({"Data":"Country"}).encode()))['Hash']
-        self.DescriptionHash = self.api.object_put(io.BytesIO(json.dumps({"Data":"Description"}).encode()))['Hash']
-        self.UTCHash = self.api.object_put(io.BytesIO(json.dumps({"Data":"UTC"}).encode()))['Hash']
 
-    #def Register(self,account,publickey,objectkey,email):
-    def Register(self,email,passwd,UTC,name,description,country):
+    def EmailInUsed(self,email):
+        return self.contract_instance.functions.EmailInUsed(email).call()
+    def AddressInUsed(self,address):
+        return self.contract_instance.functions.AddressInUsed(address).call()
+    def GetAccount(self,email,passwd):
         m = hashlib.md5()
         m.update(passwd.encode('utf-8')+email.encode('utf-8'))
         account = m.hexdigest()
+        account = self.w3.toBytes(text=account)
+        return account
+
+    #def Register(self,account,publickey,objectkey,email):
+    def Register(self,email,passwd,UTC,name,description,country):
+        if self.EmailInUsed(email):
+            return json.dumps({"status":"EmailInUsedException"})
         publickey = json.loads(UTC)['address']
+        publickey = self.w3.toChecksumAddress(publickey)
+        if self.AddressInUsed(publickey):
+            return json.dumps({"status":"AddressInUsedException"})
         Obyte = json.dumps({"Data":email}).encode()
         objectkey = self.api.object_put(io.BytesIO(Obyte))['Hash']
-        objectkey = self.api.object_patch_add_link(objectkey,name,self.NameHash)['Hash']
-        objectkey = self.api.object_patch_add_link(objectkey,country,self.CountryHash)['Hash']
-        objectkey = self.api.object_patch_add_link(objectkey,description,self.DescriptionHash)['Hash']
-        account = self.w3.toBytes(text=account)
-        publickey = self.w3.toChecksumAddress(publickey)
+        NameKey = self.api.object_put(io.BytesIO(json.dumps({"Data":name}).encode()))['Hash']
+        CountryKey = self.api.object_put(io.BytesIO(json.dumps({"Data":country}).encode()))['Hash']
+        DescriptionKey = self.api.object_put(io.BytesIO(json.dumps({"Data":description}).encode()))['Hash']
+        objectkey = self.api.object_patch_add_link(objectkey,'Name',NameKey)['Hash']
+        objectkey = self.api.object_patch_add_link(objectkey,'Country',CountryKey)['Hash']
+        objectkey = self.api.object_patch_add_link(objectkey,'Description',DescriptionKey)['Hash']
+        account = self.GetAccount(email,passwd)
         private_key = self.w3.eth.account.decrypt(UTC, passwd)
 
         unicorn_txn = self.contract_instance.functions.register(account,publickey,objectkey,email).buildTransaction({'nonce':self.w3.eth.getTransactionCount(publickey)})
@@ -54,9 +66,31 @@ class IDToyFramework:
         return self.w3.eth.sendRawTransaction(signed_txn.rawTransaction).hex()
 
     def GetUserInfo(self,email,passwd):
-        m = hashlib.md5()
-        m.update(passwd.encode('utf-8')+email.encode('utf-8'))
-        account = m.hexdigest()
-        account = self.w3.toBytes(text=account)
-        result = self.contract_instance.functions.GetUserInfo(account).call()
-        return result
+        if not self.EmailInUsed(email):
+            return json.dumps({"status":"EmailNotExistedException"})
+        account = self.GetAccount(email,passwd)
+        objectkey = self.contract_instance.functions.GetUserInfo(account).call()
+        results = self.api.object_get(objectkey)['Links']
+        Odict = dict()
+        for x in results:
+            if x['Name'] == 'Name':
+                Odict['Name'] = self.api.object_get(x['Hash'])['Data']
+            elif x['Name'] == 'Country':
+                Odict['Country'] = self.api.object_get(x['Hash'])['Data']
+            elif x['Name'] == 'Description':
+                Odict['Description'] = self.api.object_get(x['Hash'])['Data']
+        return json.dumps(Odict)
+
+    def SetUserInfo(self,email,passwd,UTC,target,value):
+        account = self.GetAccount(email,passwd)
+        ValueKey = NameKey = self.api.object_put(io.BytesIO(json.dumps({"Data":value}).encode()))['Hash']
+        objectkey = self.contract_instance.functions.GetUserInfo(account).call()
+        objectkey = self.api.object_patch_rm_link(objectkey,target)['Hash']
+        objectkey = self.api.object_patch_add_link(objectkey,target,ValueKey)['Hash']
+
+        publickey = json.loads(UTC)['address']
+        publickey = self.w3.toChecksumAddress(publickey)
+        private_key = self.w3.eth.account.decrypt(UTC, passwd)
+        unicorn_txn = self.contract_instance.functions.SetUserInfo(account,objectkey).buildTransaction({'nonce':self.w3.eth.getTransactionCount(publickey)})
+        signed_txn = self.w3.eth.account.signTransaction(unicorn_txn, private_key=private_key)
+        return self.w3.eth.sendRawTransaction(signed_txn.rawTransaction).hex()
--- contract/IdentityManager.sol
+++ contract/IdentityManager.sol
@@ -10,13 +10,43 @@ contract IdentityManager {
     function IdentityManager() public{
     }
 
+    function stringsEqual(string storage _a, string memory _b) internal constant returns (bool) {
+        bytes storage a = bytes(_a);
+        bytes memory b = bytes(_b);
+        if (a.length != b.length)
+            return false;
+        for (uint i = 0; i < a.length; i ++)
+            if (a[i] != b[i])
+                return false;
+        return true;
+    }
+
     mapping(bytes32 => Person) public user;
     mapping(address => string) public UserMapping;
+    string[] UserList;
+
+    function EmailInUsed(string email) public returns (bool){
+        for(uint i=0;i<UserList.length;i++)
+            if(stringsEqual(UserList[i],email))
+                return true;
+        return false;
+    }
+
+    function AddressInUsed(address publickey) public returns (bool){
+        if(stringsEqual(UserMapping[publickey],''))
+            return false;
+        return true;
+    }
     
     function register(bytes32 account, address publickey, string objectkey, string email){
+        if(EmailInUsed(email))
+            return;
+        if(AddressInUsed(publickey))
+            return;
 	user[account].PublicKey = publickey;
 	user[account].ObjectKey = objectkey;
         UserMapping[publickey] = email;
+        UserList.push(email);
     }
 
     function GetUserInfo(bytes32 account) public returns (string) {
